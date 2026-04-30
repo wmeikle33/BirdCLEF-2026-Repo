503186d3e46998bf12f91783e31ab5305db744d6
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-30 22:01:34 +0800

    Create download_data.py

---
+++ scripts/download_data.py
@@ -0,0 +1,24 @@
+class BirdDataset(Dataset):
+    PATH = '/kaggle/input/competitions/birdclef-2026/'
+    TEST_PATH = PATH + 'test_soundscapes/'
+    TRAIN_PATH = PATH + 'train_soundscapes/'
+    taxonomy = pd.read_csv(PATH+'taxonomy.csv')
+    
+    LABELS = list(np.unique(taxonomy.primary_label))
+    CLASSES = list(np.unique(taxonomy.class_name))
+    BATCH_SIZE = 32
+
+    DUR = 5
+    SR = 32000
+
+    def __init__(self, split_size=0.2, seed=2, n_repeat=1, is_train=True):
+        paths = [self.TEST_PATH+x for x in os.listdir(self.TEST_PATH) if '.ogg' in x]
+        if len(paths)==0:
+            paths = [self.TRAIN_PATH+x for x in os.listdir(self.TRAIN_PATH) if '.ogg' in x]
+            paths = sorted(paths)[:16]
+        df = pd.DataFrame([], index=range(len(paths)*int(60/self.DUR)), columns=['path', 'start', 'end'])
+        self.paths = paths.copy()
+
+
+    def __len__(self):
+        return len(self.paths)
